94f1727f041da5eaf0f34b94d19a533cf2c3ef65
PensCup87 <bdt9123@gmail.com>
2017-11-07 11:05:19 -0500

    Created two functions

--- scripts/JavaScript.js
+++ scripts/JavaScript.js
@@ -1,3 +1,20 @@
-﻿for (var i = 0; 1 < 10; i++) {
-    Console.log(i);
+﻿//for (var i = 1; i <= 10; i++) {
+//    console.log(i);
+//}
+//console.log("Hello");
+
+//var name = "Batman";
+
+//console.log("Hello " + name);
+
+function helloWorld(){
+    var greeting = "Hello Hello!";
+    alert(greeting);
 }
+//Call the function
+helloWorld();
+
+function add (a, b){
+    return a + b;
+}
+console.log(add(4, 5));
